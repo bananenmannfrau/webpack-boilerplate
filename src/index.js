f0b97e9a6809f1d5b5d43ef7f1400eaa5649af00
bananenmannfrau <bananenmannfrau@posteo.net>
2017-10-22 17:49:57 +0200

    adds rust for web-assembly

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log('hello world');
+const wasm = require("./main.rs")
+wasm.initialize({noExitRuntime: true}).then(module => {
+  // you can call module.cwrap here to get function wrappers for Rust functions
+})
