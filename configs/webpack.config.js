fb699266890d092763d5e6dedef65fa3110add04
bananenmannfrau <bananenmannfrau@posteo.net>
2018-02-09 18:46:41 +0100

    updates configs for react and jsx

--- configs/webpack.config.js
+++ configs/webpack.config.js
@@ -3,25 +3,28 @@ const root = path.resolve(__dirname, '..');
 
 module.exports = {
   entry: './src/index.js',
+  resolve: {
+    extensions: ['.js', '.json', '.jsx']
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(root, 'dist'),
-    publicPath: '/dist/',
+    publicPath: '/dist/'
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
-        loader: 'babel-loader',
+        loader: 'babel-loader'
       },
       {
         test: /\.css$/,
         use: [
           'style-loader',
-          {loader: 'css-loader', options: {modules: true, importLoaders: 1}},
-          'postcss-loader',
-        ],
-      },
-    ],
-  },
+          { loader: 'css-loader', options: { modules: true, importLoaders: 1 } },
+          'postcss-loader'
+        ]
+      }
+    ]
+  }
 };
