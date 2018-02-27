07d4597cd24eca678d9b5dbbffd65601ac67d8b0
Maximilian Ehlers <maximilian.ehlers@innovation.hrs.com>
2018-02-27 14:27:24 +0100

    updates to webpack 4

---
+++ configs/webpack.config.prod.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const root = path.resolve(__dirname, "..");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(root, "dist"),
+    publicPath: "/dist/"
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: { modules: true, importLoaders: 1 }
+          },
+          "postcss-loader"
+        ]
+      }
+    ]
+  }
+};
